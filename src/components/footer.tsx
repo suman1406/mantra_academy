60b76dc00f3f2e203ff20605002286cc3a59899c
Suman Panigrahi <psuman1406@gmail.com>
2025-08-09 15:03:00 +0000

    Youtube - https://www.youtube.com/@MantrAcademy24 Insta Gram - https://w

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
-import { Instagram, Youtube, Twitter } from "lucide-react";
+
+import { Instagram, Youtube, Twitter, Facebook } from "lucide-react";
 import { Logo } from "./logo";
 import { AmbientAudio } from "./ambient-audio";
 
@@ -15,9 +16,10 @@ export function Footer() {
             <AmbientAudio />
           </div>
           <div className="flex justify-center md:justify-end space-x-4">
-            <a href="#" className="text-foreground/60 hover:text-primary"><Instagram /></a>
-            <a href="#" className="text-foreground/60 hover:text-primary"><Youtube /></a>
-            <a href="#" className="text-foreground/60 hover:text-primary"><Twitter /></a>
+            <a href="https://www.instagram.com/mantr.academy/" target="_blank" rel="noopener noreferrer" className="text-foreground/60 hover:text-primary"><Instagram /></a>
+            <a href="https://www.youtube.com/@MantrAcademy24" target="_blank" rel="noopener noreferrer" className="text-foreground/60 hover:text-primary"><Youtube /></a>
+            <a href="https://x.com/namaste_mantra" target="_blank" rel="noopener noreferrer" className="text-foreground/60 hover:text-primary"><Twitter /></a>
+            <a href="https://www.facebook.com/profile.php?id=61566345014729" target="_blank" rel="noopener noreferrer" className="text-foreground/60 hover:text-primary"><Facebook /></a>
           </div>
         </div>
       </div>
