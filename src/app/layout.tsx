12a5dd0a4863508b58ad0f11efcf9200cccaa0ef
Suman Panigrahi <psuman1406@gmail.com>
2025-08-10 15:44:26 +0000

    Even in the mobile screen browser, the cards are rendering in full width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Mantra Academy</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Learn the power of mantras and transform your life." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
